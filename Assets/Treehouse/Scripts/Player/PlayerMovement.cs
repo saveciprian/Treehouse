43d150a3754c0d3b5996a4071b04f936d02362b4
Tudor Savescu <60888561+saveciprian@users.noreply.github.com>
2025-06-15 14:44:24 +0200

    added looking around & reworked movement

--- Assets/Treehouse/Scripts/Player/PlayerMovement.cs
+++ Assets/Treehouse/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
@@ -5,10 +6,14 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 30f;
+    [SerializeField] private float lookSpeed = 30f;
+    private Camera playerCamera;
     private Rigidbody rb;
     public PlayerInput PlayerControls;
     private InputAction move;
+    private InputAction look;
     private Vector2 moveDirection;
+    private Vector2 lookDirection;
     
 
     private void Awake()
@@ -19,26 +24,40 @@ public class PlayerMovement : MonoBehaviour
     private void OnEnable()
     {
         move = PlayerControls.Player.Move;
+        look = PlayerControls.Player.Look;
         move.Enable();
+        look.Enable();
     }
 
     private void OnDisable()
     {
         move.Disable();
+        look.Enable();
     }
 
     private void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
+        playerCamera = Camera.main;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void Update()
     {
         moveDirection = move.ReadValue<Vector2>();
+        moveDirection.Normalize();
+        lookDirection = look.ReadValue<Vector2>();
+
+        Debug.Log("Look Direction: " + lookDirection);
+        transform.Rotate(Vector3.up * lookDirection.x * Time.deltaTime * lookSpeed);
+        playerCamera.transform.Rotate(Vector3.left * lookDirection.y * Time.deltaTime * lookSpeed);
+
     }
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector3(moveDirection.x * movementSpeed, rb.linearVelocity.y, moveDirection.y * movementSpeed);
+        rb.MovePosition(rb.position + transform.forward * movementSpeed * Time.deltaTime * moveDirection.y);
+        rb.MovePosition(rb.position + transform.right * movementSpeed * Time.deltaTime * moveDirection.x);
     }
 }
